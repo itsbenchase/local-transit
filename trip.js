143725ca10d5354caee7642702f2f0c2a31653c1
itsbenchase <ben.chase@benchase.info>
2025-08-09 18:17:00 -0400

    test 17 with trips

--- trip.js
+++ trip.js
@@ -3,22 +3,23 @@ const tripRoute = [];
 const tripHeadsign = [];
 const tripStart = [];
 const tripStopIds = [];
+const tripStopNames = [];
 const tripStopTimes = [];
 
 var result = "no data";
 
 function funct()
 {
-  const parsedUrl = new URL(document.URL);
+  var parsedUrl = new URL(document.URL);
   console.log(parsedUrl);
 
-  const paramName = 'trip';
+  var paramName = 'trip';
   result = parsedUrl.searchParams.get(paramName);
 
-  var fips = result.substr(0, 5);
+  fips = result.substr(0, 5);
   result = result.substr(5);
   console.log(result);
-
+  
   const testAgencyUrl = ("https://www.localtransit.app/" + fips + "_trips.txt"); // provide file location
     fetch(testAgencyUrl)
       .then(r => r.text())
@@ -36,9 +37,11 @@ function funct()
           tripHeadsign.push(data.substring(0, data.indexOf(";")));
           data = data.substr(data.indexOf(";") + 1);
           tripStart.push(data.substring(0, data.indexOf(";")));
-          data = data.substr(data.indexOf(";") + 1);
           var dataIds = data.substr(1, data.indexOf("]") - 1);
           tripStopIds.push(dataIds.split(", "));
+          data = data.substr(data.indexOf(";") + 2);
+          var dataNames = data.substr(1, data.indexOf("]") - 1);
+          tripStopNames.push(dataNames.split(", "));
           data = data.substr(data.indexOf(";") + 2);
           data = data.substr(0, data.length - 1);
           tripStopTimes.push(data.split(", "));
@@ -59,7 +62,7 @@ function findTrip(result)
 
       for (let j = 0; j < tripStopTimes[i].length; j++)
       {
-        document.getElementById("stops").innerHTML += ("<br>" + tripStopTimes[i][j] + ": " + tripStopIds[i][j]);
+        document.getElementById("stops").innerHTML += ("<br>" + tripStopTimes[i][j] + ": " + tripStopNames[i][j]);
       }
     }
   }
